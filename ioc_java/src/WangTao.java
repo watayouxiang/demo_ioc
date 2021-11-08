271d95d799ac735bdc93f1fe6c7d104a7877babb
TaoWang <watayouxiang@qq.com>
2021-11-08 15:45:06 +0800

    empty

--- ioc_java/src/WangTao.java
+++ ioc_java/src/WangTao.java
@@ -2,17 +2,52 @@ import sun.misc.ProxyGenerator;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 public class WangTao {
     public static void main(String[] args) throws Exception {
-        demo();
+//        generateProxyClass();
+//        staticProxy();
+        dynamicProxy();
+    }
 
+    /**
+     * 动态代理
+     */
+    private static void dynamicProxy() {
+        HelloImpl hello = new HelloImpl();
+        HelloInterface proxy = (HelloInterface) Proxy.newProxyInstance(
+                // ClassLoader loader
+                hello.getClass().getClassLoader(),
+                // Class<?>[] interfaces
+                hello.getClass().getInterfaces(),
+                // InvocationHandler h
+                new InvocationHandler() {
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        System.out.println("before invoke " + method.getName());
+                        Object o = method.invoke(hello, args);
+                        System.out.println("after invoke " + method.getName());
+                        return o;
+                    }
+                });
+        proxy.sayHello();
+    }
+
+    /**
+     * 静态代理
+     */
+    private static void staticProxy() {
+        HelloProxy proxy = new HelloProxy();
+        proxy.sayHello();
     }
 
     /**
      * 生成代理类 class
      */
-    private static void demo() throws Exception {
+    private static void generateProxyClass() throws Exception {
         // 生成一个 class 二进制文件
         byte[] bytes = ProxyGenerator.generateProxyClass("HelloImpl", new Class<?>[]{HelloInterface.class});
 
@@ -25,4 +60,27 @@ public class WangTao {
         outputStream.flush();
         outputStream.close();
     }
+
+    public static class HelloProxy implements HelloInterface {
+        private final HelloInterface helloInterface = new HelloImpl();
+
+        @Override
+        public void sayHello() {
+            System.out.println("before hello ~");
+            helloInterface.sayHello();
+            System.out.println("after hello ~");
+        }
+    }
+
+    public static class HelloImpl implements HelloInterface {
+        @Override
+        public void sayHello() {
+            System.out.println("hello ~");
+        }
+    }
+
+    public interface HelloInterface {
+        void sayHello();
+    }
+
 }
