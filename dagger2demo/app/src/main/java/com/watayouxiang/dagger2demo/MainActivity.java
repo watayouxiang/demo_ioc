cedc25dcc7262d547335861604ea63403a46d3dd
TaoWang <watayouxiang@qq.com>
2021-11-08 20:27:18 +0800

    empty

--- dagger2demo/app/src/main/java/com/watayouxiang/dagger2demo/MainActivity.java
+++ dagger2demo/app/src/main/java/com/watayouxiang/dagger2demo/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.watayouxiang.demo.DemoActivity;
+import com.watayouxiang.demo1.Demo1Activity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +20,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void demoClick(View view) {
         startActivity(new Intent(this, DemoActivity.class));
+    }
+
+    public void demoClick1(View view) {
+        startActivity(new Intent(this, Demo1Activity.class));
     }
 }
---
+++ dagger2demo/demo1/src/main/java/com/watayouxiang/demo1/Demo1Activity.java
@@ -0,0 +1,14 @@
+package com.watayouxiang.demo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Demo1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo1);
+    }
+}
