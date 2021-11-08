b3406848bb5b8f3bed36fe6f41917d6df1323384
TaoWang <watayouxiang@qq.com>
2021-11-08 19:53:44 +0800

    empty

--- dagger2demo/app/src/main/java/com/watayouxiang/dagger2demo/MainActivity.java
+++ dagger2demo/app/src/main/java/com/watayouxiang/dagger2demo/MainActivity.java
@@ -8,8 +8,9 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.watayouxiang.dagger2demo.component.DaggerMainComponent;
+import com.watayouxiang.dagger2demo.module.HttpModule;
+import com.watayouxiang.dagger2demo.module.ImModule;
 import com.watayouxiang.dagger2demo.object.HttpClient;
-import com.watayouxiang.dagger2demo.object.ImClient;
 
 import javax.inject.Inject;
 
@@ -19,22 +20,34 @@ public class MainActivity extends AppCompatActivity {
     HttpClient httpClient;
 
     @Inject
-    ImClient imClient;
+    HttpClient httpClient2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        DaggerMainComponent.create().injectMainActivity(this);
-
-        TextView tv_text = findViewById(R.id.tv_text);
-        tv_text.setText(httpClient.toString());
-        tv_text.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.tv_text).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, MainActivity2.class));
             }
         });
+
+        // 注解第一种方式
+        DaggerMainComponent.create().injectMainActivity(this);
+
+        // 注解第二种方式
+        if (false) {
+            DaggerMainComponent.builder()
+                    .httpModule(new HttpModule())
+                    .imModule(new ImModule())
+                    .build()
+                    .injectMainActivity(this);
+        }
+
+        TextView tv_text = findViewById(R.id.tv_text);
+        tv_text.setText(httpClient.toString());
+        tv_text.append("\n\n");
+        tv_text.append(httpClient2.toString());
     }
 }
--- dagger2demo/app/src/main/java/com/watayouxiang/dagger2demo/MainActivity2.java
+++ dagger2demo/app/src/main/java/com/watayouxiang/dagger2demo/MainActivity2.java
@@ -1,37 +1,15 @@
 package com.watayouxiang.dagger2demo;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.watayouxiang.dagger2demo.component.DaggerMainComponent;
-import com.watayouxiang.dagger2demo.module.HttpModule;
-import com.watayouxiang.dagger2demo.module.ImModule;
-import com.watayouxiang.dagger2demo.object.HttpClient;
-import com.watayouxiang.dagger2demo.object.ImClient;
-
-import javax.inject.Inject;
-
 public class MainActivity2 extends AppCompatActivity {
-    @Inject
-    HttpClient httpClient;
-
-    @Inject
-    ImClient imClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        DaggerMainComponent.builder()
-                .httpModule(new HttpModule())
-                .imModule(new ImModule())
-                .build()
-                .injectMainActivity2(this);
-
-        TextView tv_text = findViewById(R.id.tv_text);
-        tv_text.setText(httpClient.toString());
     }
 }
